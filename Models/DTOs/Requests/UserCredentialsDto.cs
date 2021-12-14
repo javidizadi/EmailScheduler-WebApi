88bea1237c0fb50152e145516958d2aa1d9c57c2
Javid <none>
2021-12-14 13:53:48 +0330

    [Rename] UserRegistrationDto.cs to UserCredentialsDto.cs

--- Models/DTOs/Requests/UserCredentialsDto.cs
+++ Models/DTOs/Requests/UserCredentialsDto.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Email_Scheduler_WebApi.Models.DTOs.Requests;
 
-public class UserRegistrationDto
+public class UserCredentialsDto
 {
     [Required] [EmailAddress] public string? Email { get; set; }
 
