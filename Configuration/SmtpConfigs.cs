a56b97a66e847cc1658b62e34fd3c451d8b180c7
Javid <none>
2021-12-16 11:06:29 +0330

    [Fix] Change Props Nullable

--- Configuration/SmtpConfigs.cs
+++ Configuration/SmtpConfigs.cs
@@ -2,13 +2,13 @@ namespace Email_Scheduler_WebApi.Configuration;
 
 public class SmtpConfigs
 {
-    public string Host { get; set; }
+    public string? Host { get; set; }
 
     public int Port { get; set; }
 
-    public string From { get; set; }
+    public string? From { get; set; }
 
-    public string Username { get; set; }
+    public string? Username { get; set; }
 
-    public string Password { get; set; }
+    public string? Password { get; set; }
 }
